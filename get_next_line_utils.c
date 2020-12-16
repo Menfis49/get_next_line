92edc28270d6d035f549a2102aa4881e330109de
vazra <valentin.azra@gmail.com>
2020-12-16 21:11:49 +0100

    test

---
+++ get_next_line_utils.c
@@ -0,0 +1,76 @@
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t		i;
+	unsigned char	*a;
+	unsigned char	*b;
+
+	i = 0;
+	a = (unsigned char *)dest;
+	b = (unsigned char *)src;
+	if (a > b)
+		while (i < n)
+		{
+			a[n - 1] = b[n - 1];
+			n--;
+		}
+	else
+		while (i < n)
+		{
+			a[i] = b[i];
+			i++;
+		}
+	return (dest);
+}
+
+char	*join_str(char const *s1, char const *s2)
+{
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	stot_len;
+	char	*rtn;
+
+	if (!s1 && !s2)
+		return (0);
+	s1_len = ft_strlen((char *)s1);
+	s2_len = ft_strlen((char *)s2);
+	stot_len = s1_len + s2_len + 1;
+	rtn = malloc(sizeof(char) * stot_len);
+	if (!rtn)
+		return (0);
+	ft_memmove(rtn, s1, s1_len);
+	ft_memmove(rtn + s1_len, s2, s2_len);
+	rtn[stot_len - 1] = '\0';
+	free((char *)s1);
+	return (rtn);
+}
+
+int	has_return(char *str)
+{
+	int i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
