077c9e71df474ea2c015ed651c29487bc8bc61af
vazra <valentin.azra@gmail.com>
2020-12-17 06:37:32 +0100

    test2

--- get_next_line.c
+++ get_next_line.c
@@ -78,3 +78,97 @@ int	get_next_line(int fd, char **line)
 		return (0);
 	return (1);
 }
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*ptr;
+	size_t	i;
+
+	if (!n)
+		return ;
+	ptr = s;
+	i = 0;
+	while (i < n)
+		*(ptr + i++) = 0;
+}
+
+char	*ft_strnew(size_t size)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * size + 1);
+	if (!str)
+		return (NULL);
+	ft_bzero(str, size + 1);
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_str;
+	size_t i;
+	size_t j;
+	size_t s1_len;
+	size_t s2_len;
+	
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_str = ft_strnew(s1_len + s2_len);
+	if (!new_str)
+		return (NULL);
+	i = -1;
+	j = -1;
+	while (++i < s1_len)
+		*(new_str + i) = *(s1 + i);
+	while (++j < s2_len)
+		*(new_str + i++) = *(s2 + j);
+	return (new_str);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char const *s, int fd)
+{
+	while (s && *s)
+		ft_putchar_fd(*s++, fd);
+}
+
+void	ft_putendl_fd(char const *s, int fd)
+{
+	ft_putstr_fd(ft_strjoin(s, "\n"), fd);
+}
+
+void	ft_putendl(char const *s)
+{
+	ft_putendl_fd(s, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;	
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
+	if (argc == 2)
+		close(fd);
+}
